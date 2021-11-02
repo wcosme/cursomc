4330e094d1d158648588c5361c0a466c38010c8a
Wallace Gonçalves <wallacecosme@gmail.com>
2021-11-02 19:37:29 -0300

    Banco de dados H2

---
+++ src/main/java/br/com/wgsolution/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,16 @@
+package br.com.wgsolution.cursomc.resources;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public String listar() {
+		return "Serviço REST no ar.";
+	}
+
+}

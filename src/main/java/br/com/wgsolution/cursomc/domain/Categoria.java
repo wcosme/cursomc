06d4112a96df3b984faba2d28b666fe1f3867edb
Wallace Gonçalves <wallacecosme@gmail.com>
2021-12-05 19:45:07 -0300

    Produto e associação muitos para muitos

--- src/main/java/br/com/wgsolution/cursomc/domain/Categoria.java
+++ src/main/java/br/com/wgsolution/cursomc/domain/Categoria.java
@@ -1,12 +1,15 @@
 package br.com.wgsolution.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +20,9 @@ public class Categoria implements Serializable {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<Produto>() ;
 	
 	public Categoria() {
 
@@ -43,10 +49,18 @@ public class Categoria implements Serializable {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}	
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, nome);
+		return Objects.hash(id);
 	}
 
 	@Override
@@ -58,7 +72,6 @@ public class Categoria implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome);
-	}	
-
+		return Objects.equals(id, other.id);
+	}
 }
--- src/main/java/br/com/wgsolution/cursomc/domain/Produto.java
+++ src/main/java/br/com/wgsolution/cursomc/domain/Produto.java
@@ -1,21 +1,34 @@
 package br.com.wgsolution.cursomc.domain;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "TB_PRODUTO")
-public class Produto {
+public class Produto implements Serializable {
+	private static final long serialVersionUID = 1L;
 	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private Double preco;
+	
+	@ManyToMany
+	@JoinTable(name = "PRODUTO_CATEGORIA", 
+		joinColumns = @JoinColumn(name = "produto_id"), 
+		inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private List<Categoria> categorias = new ArrayList<Categoria>();
 	
 	public Produto() {
 		
@@ -55,11 +68,17 @@ public class Produto {
 		this.preco = preco;
 	}
 
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
 
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
+	}	
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id, nome, preco);
+		return Objects.hash(id);
 	}
 
 
@@ -72,7 +91,7 @@ public class Produto {
 		if (getClass() != obj.getClass())
 			return false;
 		Produto other = (Produto) obj;
-		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome) && Objects.equals(preco, other.preco);
-	}	
-
+		return Objects.equals(id, other.id);
+	}
+	
 }

ee04bf1370b1f372622550622a2cc19a9405196f
Wallace Gonçalves <wallacecosme@gmail.com>
2021-12-05 19:20:12 -0300

    Implementando a classe Produto

---
+++ src/main/java/br/com/wgsolution/cursomc/domain/Produto.java
@@ -0,0 +1,78 @@
+package br.com.wgsolution.cursomc.domain;
+
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUTO")
+public class Produto {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private Double preco;
+	
+	public Produto() {
+		
+	}	
+	
+
+	public Produto(Long id, String nome, Double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, nome, preco);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id) && Objects.equals(nome, other.nome) && Objects.equals(preco, other.preco);
+	}	
+
+}
